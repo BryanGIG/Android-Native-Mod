0f72591305b7bd2f5fd61da8c94be9b5ad15ef15
BryanGIG <contacts.bryangig@gmail.com>
2024-02-24 12:30:45 +0700

    fix: wait lib in another thread

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,17 +1,20 @@
 #include <jni.h>
 #include <string>
+#include <thread>
 #include "Engine/Includes.hpp"
 
 __attribute__ ((constructor))
 void lib_main() {
-    ProcMap map;
-    do {
-        map = KittyMemory::getElfBaseMap("libil2cpp.so");
-    } while (!map.isValid() && !map.isValidELF());
+    std::thread([]() {
+        ProcMap map;
+        do {
+            map = KittyMemory::getElfBaseMap("libil2cpp.so");
+        } while (!map.isValid() && !map.isValidELF());
 
-    // Initialize the classes
-    ListCBaseEntry::add({
-        new CheatDetector()
-    });
-    ListCBaseEntry::Initialize();
+        // Initialize the classes
+        ListCBaseEntry::add({
+                                    new CheatDetector()
+                            });
+        ListCBaseEntry::Initialize();
+    }).detach();
 }
